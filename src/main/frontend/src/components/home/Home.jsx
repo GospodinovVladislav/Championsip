d8625d915b3c6500e5273802a8642a4b3055b2c4
Vladislav Gospodinov <vladislav.gospodinov@yahoo.com>
2018-12-30 13:18:17 +0200

    First Commit

---
+++ src/main/frontend/src/components/home/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => (<div>Home Directory</div>)
+
+export default Home;
---
+++ src/main/java/com/vgospodinov/championship/ChampionshipApplication.java
@@ -0,0 +1,14 @@
+package com.vgospodinov.championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionshipApplication.class, args);
+	}
+
+}
+
